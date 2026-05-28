560a64409540a179fe35d1d1fa60e1ee975d809d
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-05-28 12:55:38 +0530

    Create Convert min Heap to max Heap.cpp

---
+++ Convert min Heap to max Heap.cpp
@@ -0,0 +1,27 @@
+void maxheapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        maxheapify(arr, n, largest);
+    }
+}
+
+vector<int> mintomax(vector<int>& arr) {
+    int n = arr.size();
+
+   
+    for (int i = n/2 - 1; i >= 0; i--) {
+        maxheapify(arr, n, i);
+    }
+
+    return arr;
+}
