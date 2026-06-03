5fd215cae738a2004e8c7237f776253f8c9cdd4b
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-06-03 17:37:29 +0530

    Create DiameterOfTree.cpp

---
+++ DiameterOfTree.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    int maxdia=0;
+    int levels(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+max(levels(root->left),levels(root->right));
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root == NULL) return 0;
+        int dia = levels(root->left)+levels(root->right);
+        maxdia =max(maxdia,dia);
+        diameterOfBinaryTree(root->left);
+        diameterOfBinaryTree(root->right);
+        return maxdia;
+        
+        
+    }
+};
