259ebb8239ce19775308b89f9e85cb6cc8f62ff4
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-07-08 11:00:55 +0530

    Time: 3 ms (0.35%), Space: 7.7 MB (87.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp) {
+    int dp[46];  
+
+    int solve(int n) {
         if (n <= 2) return n;
 
-        
         if (dp[n] != -1) return dp[n];
 
-       
-        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+        return dp[n] = solve(n - 1) + solve(n - 2);
     }
 
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        memset(dp, -1, sizeof(dp));  
+        return solve(n);
     }
 };
