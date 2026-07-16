033fe85d3b3b10c3e1559a7700b493939ec8fb3f
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-07-16 15:34:37 +0530

    Time: 3 ms (8.64%), Space: 8.9 MB (36.01%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     bool checkIfPangram(string sen) {
-        int n=sen.length();
-        set<char>s;
-
-        for(int i=0;i<n;i++){
-            s.insert((int)sen[i]);
-        }
-        for(int i=97 ;i<=122;i++){
-            if(s.find(i)==s.end()) return false;
-        }
-        return true;
-        
+        set<char> s(sen.begin(), sen.end());
+        return s.size() == 26;
     }
 };
