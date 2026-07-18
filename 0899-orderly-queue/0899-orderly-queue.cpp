62b57206b89932d82d2feacd76e0d113794f94ab
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-07-18 11:37:44 +0530

    Time: 4 ms (25.11%), Space: 13.6 MB (38.12%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k>1){
+            sort(s.begin(),s.end());
+            return s;
+
+        }
+        string res=s;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            string temp=s.substr(i)+s.substr(0,i);
+            res=min(res,temp);
+        }
+        return res;        
+    }
+};
