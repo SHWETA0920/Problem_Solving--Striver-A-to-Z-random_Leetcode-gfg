8d4bfcc48ccf8ebef06c20d00be63d261a0edbec
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-07-30 13:12:41 +0530

    Time: 0 ms (100%), Space: 17.9 MB (90.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -4,13 +4,12 @@ public:
         int n = image.size();
         int m = image[0].size();
 
-        if (r < 0 || c < 0 || r >= n || c >= m || image[r][c] != oldColor)
-            return;
+        if (r < 0 || c < 0 || r >= n || c >= m || image[r][c] != oldColor) return;
         image[r][c] = newColor;
-        dfs(image, r+1, c, oldColor, newColor);
-        dfs(image, r-1, c, oldColor, newColor);
-        dfs(image, r, c+1, oldColor, newColor);
-        dfs(image, r, c-1, oldColor, newColor);
+        dfs(image, r+1, c, oldColor, newColor); // go down
+        dfs(image, r-1, c, oldColor, newColor); // go up
+        dfs(image, r, c+1, oldColor, newColor); // go right
+        dfs(image, r, c-1, oldColor, newColor); // go left
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
