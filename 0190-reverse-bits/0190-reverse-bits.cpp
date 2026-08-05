5e23fc85fd6e37807482a0cbd281b4ac67f32404
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-08-05 22:18:56 +0530

    Time: 0 ms (100%), Space: 8.1 MB (73.38%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int reverseBits(int n) {
+        bitset<32> binary(n);
+        unsigned int result = binary.to_ulong();
+
         unsigned int ans = 0;
 
-        for (int i = 0; i < 32; i++) {
-            int bit = n & 1;          
-            ans = (ans << 1) | bit;   
-            n = n >> 1;              
+        for (int i = 0; i < 32; i++) {        
+            int bit = n & 1;
+            ans = (ans << 1) | bit;
+            n = n >> 1;
         }
 
         return ans;
